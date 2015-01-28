becf44f35aa20ae540226c5771187b882ca63d94
William E. Karnesky <William.Karnesky@pnnl.gov>
2015-01-27 16:16:21 -0800

    Started changing the working version to a combined view and viewmodel

---
+++ Atreyu/ViewModels/CombinedHeatmapViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace Atreyu.ViewModels
+{
+    using System.ComponentModel.Composition;
+
+    [Export]
+    public class CombinedHeatmapViewModel
+    {
+
+        public CombinedHeatmapViewModel()
+        {
+            
+        }
+
+
+        /// <summary>
+        /// TODO The frame manipulation view model.
+        /// </summary>
+        public FrameManipulationViewModel frameManipulationViewModel { get; private set; }
+
+        /// <summary>
+        /// TODO The heat map view model.
+        /// </summary>
+        public HeatMapViewModel heatMapViewModel { get; private set; }
+
+        /// <summary>
+        /// TODO The mz spectra view model.
+        /// </summary>
+        public MzSpectraViewModel mzSpectraViewModel { get; private set; }
+
+        /// <summary>
+        /// TODO The total ion chromatogram view model.
+        /// </summary>
+        public TotalIonChromatogramViewModel totalIonChromatogramViewModel { get; private set; }
+
+    }
+}
---
+++ Atreyu/Views/CombinedHeatmapView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Atreyu.Views
+{
+    using Atreyu.ViewModels;
+
+    using ReactiveUI;
+
+    /// <summary>
+    /// Interaction logic for CombinedHeatmapView.xaml
+    /// </summary>
+    public partial class CombinedHeatmapView : UserControl, IViewFor<CombinedHeatmapViewModel>
+    {
+        public CombinedHeatmapView() : this(new CombinedHeatmapViewModel())
+        {
+        }
+
+
+        public CombinedHeatmapView(CombinedHeatmapViewModel viewModel)
+        {
+            InitializeComponent();
+            this.ViewModel = viewModel;
+        }
+
+        object IViewFor.ViewModel
+        {
+            get
+            {
+                return this.ViewModel;
+            }
+            set
+            {
+                this.ViewModel = value as CombinedHeatmapViewModel;
+            }
+        }
+
+        public CombinedHeatmapViewModel ViewModel { get; set; }
+    }
+}
