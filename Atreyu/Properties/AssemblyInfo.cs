4a93a17615e86c8b5ac85012946f00be2ee94133
William E. Karnesky <William.Karnesky@pnnl.gov>
2015-02-05 15:09:52 -0800

    Update Version to 1.1

--- Atreyu/Properties/AssemblyInfo.cs
+++ Atreyu/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Windows;
 [assembly: AssemblyTitle("Atreyu")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Pacific Northwest National Laboratory")]
 [assembly: AssemblyProduct("Atreyu")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -54,4 +54,4 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
