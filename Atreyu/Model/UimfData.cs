PNNL-Comp-Mass-Spec/Atreyu

commit d8e2aa5e73880ddb1f4d4fb317c7ab995d08c2ca

Add a check for null frameParams due to an invalid frame number

Atreyu/Model/UimfData.cs
@@ -560,38 +560,45 @@ namespace Atreyu.Models
             this.EndFrameNumber = endFrameNumber;
 
             var frameParams = this._dataReader.GetFrameParams(startFrameNumber);
-
-            this.FrameSlope = frameParams.GetValueDouble(FrameParamKeyType.CalibrationSlope);
-            this.FrameIntercept = frameParams.GetValueDouble(FrameParamKeyType.CalibrationIntercept);
-
-            this.FrameType = frameParams.GetValue(FrameParamKeyType.FrameType);
-            
-            var temp = this._dataReader.AccumulateFrameData(
-                startFrameNumber, 
-                endFrameNumber, 
-                false, 
-                this.StartScan, 
-                this.EndScan, 
-                startBin, 
-                endBin, 
-                (int)this.ValuesPerPixelX,
-                (int)this.ValuesPerPixelY);
-
-            var arrayLength = (int)Math.Round((endBin - startBin + 1) / this.ValuesPerPixelY);
-
-            var tof = new double[arrayLength];
-            var mz = new double[arrayLength];
-            var calibrator = this._dataReader.GetMzCalibrator(frameParams);
-
-            for (var i = 0; i < arrayLength; i++)
+            if (frameParams == null)
             {
-                tof[i] = this._dataReader.GetPixelMZ(i);
-                mz[i] = calibrator.TOFtoMZ(tof[i] * 10);
+                // Frame number is out of range
+                this.FrameData = new double[0, 0];
             }
+            else
+            {
+                this.FrameSlope = frameParams.GetValueDouble(FrameParamKeyType.CalibrationSlope);
+                this.FrameIntercept = frameParams.GetValueDouble(FrameParamKeyType.CalibrationIntercept);
 
-            this.BinToMzMap = mz;
+                this.FrameType = frameParams.GetValue(FrameParamKeyType.FrameType);
 
-            this.FrameData = temp;
+                var temp = this._dataReader.AccumulateFrameData(
+                    startFrameNumber,
+                    endFrameNumber,
+                    false,
+                    this.StartScan,
+                    this.EndScan,
+                    startBin,
+                    endBin,
+                    (int)this.ValuesPerPixelX,
+                    (int)this.ValuesPerPixelY);
+
+                var arrayLength = (int)Math.Round((endBin - startBin + 1) / this.ValuesPerPixelY);
+
+                var tof = new double[arrayLength];
+                var mz = new double[arrayLength];
+                var calibrator = this._dataReader.GetMzCalibrator(frameParams);
+
+                for (var i = 0; i < arrayLength; i++)
+                {
+                    tof[i] = this._dataReader.GetPixelMZ(i);
+                    mz[i] = calibrator.TOFtoMZ(tof[i] * 10);
+                }
+
+                this.BinToMzMap = mz;
+
+                this.FrameData = temp;
+            }
 
             this.GateData();
 

Atreyu.sln
@@ -11,26 +11,32 @@ Global
 	GlobalSection(SolutionConfigurationPlatforms) = preSolution
 		Debug|Any CPU = Debug|Any CPU
 		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
 		Release|Any CPU = Release|Any CPU
 		Release|x64 = Release|x64
+		Release|x86 = Release|x86
 	EndGlobalSection
 	GlobalSection(ProjectConfigurationPlatforms) = postSolution
 		{DE31E91F-CB9A-4C68-8F0B-C90BEF94FCEF}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
 		{DE31E91F-CB9A-4C68-8F0B-C90BEF94FCEF}.Debug|Any CPU.Build.0 = Debug|Any CPU
 		{DE31E91F-CB9A-4C68-8F0B-C90BEF94FCEF}.Debug|x64.ActiveCfg = Debug|x64
 		{DE31E91F-CB9A-4C68-8F0B-C90BEF94FCEF}.Debug|x64.Build.0 = Debug|x64
+		{DE31E91F-CB9A-4C68-8F0B-C90BEF94FCEF}.Debug|x86.ActiveCfg = Debug|Any CPU
 		{DE31E91F-CB9A-4C68-8F0B-C90BEF94FCEF}.Release|Any CPU.ActiveCfg = Release|Any CPU
 		{DE31E91F-CB9A-4C68-8F0B-C90BEF94FCEF}.Release|Any CPU.Build.0 = Release|Any CPU
 		{DE31E91F-CB9A-4C68-8F0B-C90BEF94FCEF}.Release|x64.ActiveCfg = Release|x64
 		{DE31E91F-CB9A-4C68-8F0B-C90BEF94FCEF}.Release|x64.Build.0 = Release|x64
+		{DE31E91F-CB9A-4C68-8F0B-C90BEF94FCEF}.Release|x86.ActiveCfg = Release|Any CPU
 		{28B1F0B8-4897-470A-8252-74A4C079E2BD}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
 		{28B1F0B8-4897-470A-8252-74A4C079E2BD}.Debug|Any CPU.Build.0 = Debug|Any CPU
 		{28B1F0B8-4897-470A-8252-74A4C079E2BD}.Debug|x64.ActiveCfg = Debug|x64
 		{28B1F0B8-4897-470A-8252-74A4C079E2BD}.Debug|x64.Build.0 = Debug|x64
+		{28B1F0B8-4897-470A-8252-74A4C079E2BD}.Debug|x86.ActiveCfg = Debug|Any CPU
 		{28B1F0B8-4897-470A-8252-74A4C079E2BD}.Release|Any CPU.ActiveCfg = Release|Any CPU
 		{28B1F0B8-4897-470A-8252-74A4C079E2BD}.Release|Any CPU.Build.0 = Release|Any CPU
 		{28B1F0B8-4897-470A-8252-74A4C079E2BD}.Release|x64.ActiveCfg = Release|x64
 		{28B1F0B8-4897-470A-8252-74A4C079E2BD}.Release|x64.Build.0 = Release|x64
+		{28B1F0B8-4897-470A-8252-74A4C079E2BD}.Release|x86.ActiveCfg = Release|Any CPU
 	EndGlobalSection
 	GlobalSection(SolutionProperties) = preSolution
 		HideSolutionNode = FALSE

Atreyu/Atreyu.csproj
@@ -179,7 +179,8 @@
     <Reference Include="System.Xaml">
       <RequiredTargetFramework>4.0</RequiredTargetFramework>
     </Reference>
-    <Reference Include="UIMFLibrary">
+    <Reference Include="UIMFLibrary, Version=3.0.5522.27194, Culture=neutral, processorArchitecture=MSIL">
+      <SpecificVersion>False</SpecificVersion>
       <HintPath>..\lib\UimfLibrary\UIMFLibrary.dll</HintPath>
     </Reference>
     <Reference Include="WindowsBase" />
@@ -274,10 +275,13 @@
   <ItemGroup>
     <None Include="Resources\Icon1.ico" />
   </ItemGroup>
-  <ItemGroup />
   <Import Project="$(MSBuildToolsPath)\Microsoft.CSharp.targets" />
   <PropertyGroup>
-    <PostBuildEvent>xcopy /Y /S /E "$(SolutionDir)lib\UimfLibrary\*"  "$(TargetDir)"</PostBuildEvent>
+    <PostBuildEvent>
+    </PostBuildEvent>
+  </PropertyGroup>
+  <PropertyGroup>
+    <PreBuildEvent>xcopy /Y /S /E /D "$(SolutionDir)lib\UimfLibrary\*"  "$(TargetDir)"</PreBuildEvent>
   </PropertyGroup>
   <!-- To modify your build process, add your task inside one of the targets below and uncomment it. 
        Other similar extension points exist, see Microsoft.Common.targets.

Viewer/Viewer.csproj
@@ -140,7 +140,7 @@
     <Reference Include="System.Xaml">
       <RequiredTargetFramework>4.0</RequiredTargetFramework>
     </Reference>
-    <Reference Include="UIMFLibrary, Version=2.1.5420.28608, Culture=neutral, processorArchitecture=AMD64">
+    <Reference Include="UIMFLibrary, Version=3.0.5522.27194, Culture=neutral, processorArchitecture=MSIL">
       <SpecificVersion>False</SpecificVersion>
       <HintPath>..\lib\UimfLibrary\UIMFLibrary.dll</HintPath>
     </Reference>
@@ -223,7 +223,11 @@
   </ItemGroup>
   <Import Project="$(MSBuildToolsPath)\Microsoft.CSharp.targets" />
   <PropertyGroup>
-    <PostBuildEvent>xcopy /Y /S /E "$(SolutionDir)lib\UimfLibrary\*"  "$(TargetDir)"</PostBuildEvent>
+    <PostBuildEvent>
+    </PostBuildEvent>
+  </PropertyGroup>
+  <PropertyGroup>
+    <PreBuildEvent>xcopy /Y /S /E /D "$(SolutionDir)lib\UimfLibrary\*"  "$(TargetDir)"</PreBuildEvent>
   </PropertyGroup>
   <!-- To modify your build process, add your task inside one of the targets below and uncomment it. 
        Other similar extension points exist, see Microsoft.Common.targets.

commit 4ca294319b4d7d809cbffb7ef10277ebb60ee435

Something in commit d8e2aa5 caused files to not even load, attempts to debug from file load didn't even allow me to step through.  So reverted that commit and am added the changes to see at what point it starts working again.

Atreyu/Model/UimfData.cs
@@ -560,38 +560,46 @@ namespace Atreyu.Models
             this.EndFrameNumber = endFrameNumber;
 
             var frameParams = this._dataReader.GetFrameParams(startFrameNumber);
+            if (frameParams == null)
+            {
+                // Frame number is out of range
+                this.FrameData = new double[0, 0];
+            }
+            else
+            {
+                this.FrameSlope = frameParams.GetValueDouble(FrameParamKeyType.CalibrationSlope);
+                this.FrameIntercept = frameParams.GetValueDouble(FrameParamKeyType.CalibrationIntercept);
 
-            this.FrameSlope = frameParams.GetValueDouble(FrameParamKeyType.CalibrationSlope);
+                this.FrameType = frameParams.GetValue(FrameParamKeyType.FrameType);
             this.FrameIntercept = frameParams.GetValueDouble(FrameParamKeyType.CalibrationIntercept);
 
-            this.FrameType = frameParams.GetValue(FrameParamKeyType.FrameType);
-            
-            var temp = this._dataReader.AccumulateFrameData(
-                startFrameNumber, 
-                endFrameNumber, 
-                false, 
-                this.StartScan, 
-                this.EndScan, 
-                startBin, 
-                endBin, 
-                (int)this.ValuesPerPixelX,
-                (int)this.ValuesPerPixelY);
+                var temp = this._dataReader.AccumulateFrameData(
+                    startFrameNumber,
+                    endFrameNumber,
+                    false,
+                    this.StartScan,
+                    this.EndScan,
+                    startBin,
+                    endBin,
+                    (int)this.ValuesPerPixelX,
+                    (int)this.ValuesPerPixelY);
 
-            var arrayLength = (int)Math.Round((endBin - startBin + 1) / this.ValuesPerPixelY);
+                var arrayLength = (int)Math.Round((endBin - startBin + 1) / this.ValuesPerPixelY);
 
-            var tof = new double[arrayLength];
-            var mz = new double[arrayLength];
-            var calibrator = this._dataReader.GetMzCalibrator(frameParams);
+                var tof = new double[arrayLength];
+                var mz = new double[arrayLength];
+                var calibrator = this._dataReader.GetMzCalibrator(frameParams);
 
-            for (var i = 0; i < arrayLength; i++)
-            {
-                tof[i] = this._dataReader.GetPixelMZ(i);
-                mz[i] = calibrator.TOFtoMZ(tof[i] * 10);
+                for (var i = 0; i < arrayLength; i++)
+                {
+                    tof[i] = this._dataReader.GetPixelMZ(i);
+                    mz[i] = calibrator.TOFtoMZ(tof[i] * 10);
+                }
+
+                this.BinToMzMap = mz;
+
+                this.FrameData = temp;
             }
-
-            this.BinToMzMap = mz;
-
-            this.FrameData = temp;
 
             this.GateData();
 