b0235f3a2aff093e647d0cc13cb2b3265f48f2da
William E. Karnesky <William.Karnesky@pnnl.gov>
2015-03-18 13:48:51 -0700

    updated framework to 4.5.1  have file loaded and single frame fetching implemented, need to save data now.

--- UIMF Data Extractor/Program.cs
+++ UIMF Data Extractor/Program.cs
@@ -8,6 +8,8 @@ namespace UimfDataExtractor
 {
     using System.IO;
 
+    using Atreyu.Models;
+
     class Program
     {
         private static CommandLineOptions options;
@@ -64,18 +66,22 @@ namespace UimfDataExtractor
             }
         }
 
-        private static async Task ProcessAllUimfInDirectory(DirectoryInfo directory)
+        private static Task ProcessAllUimfInDirectory(DirectoryInfo directory)
         {
-            if (directory.Exists)
-            {
-                var uimfs = directory.EnumerateFiles("*.uimf");
+            return Task.Run(
+                () =>
+                    {
+                        if (directory.Exists)
+                        {
+                            var uimfs = directory.EnumerateFiles("*.uimf");
 
-                
-            }
-            else
-            {
-                PrintNotFoundError(directory);
-            }
+                            Parallel.ForEach(uimfs, async uimf => await ProcessUimf(uimf));
+                        }
+                        else
+                        {
+                            PrintNotFoundError(directory);
+                        }
+                    });
         }
 
         private static void PrintNotFoundError(string fileOrDirectory)
@@ -88,16 +94,42 @@ namespace UimfDataExtractor
             Console.WriteLine(fileOrDirectory.FullName + "does not exist");
         }
         
-        private static async Task ProcessUimf(string file)
+        private static async Task ProcessUimf(FileInfo file)
         {
+            if (!file.Exists)
+            {
+                PrintNotFoundError(file);
+                return;
+            }
+
+            var uimf = new UimfData(file.FullName);
+
             if (options.AllFrames)
             {
                 Console.WriteLine("This feature is not yet implemented");
             }
             else
             {
-                
+                await ProcessFrame(uimf);
             }
         }
+
+
+        private static async Task ProcessFrame(UimfData uimf, int frameNumber = 1)
+        {
+            var data = await uimf.ReadData(
+                1,
+                uimf.MaxBins,
+                frameNumber,
+                frameNumber,
+                uimf.MaxBins, // we don't want the data compressed
+                uimf.Scans, // so we set the height and width to the scans so it is 1 value per pixel
+                0,
+                uimf.Scans,
+                false);
+
+
+        }
+
     }
 }
